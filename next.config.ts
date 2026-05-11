5dbbbe38a418c375b31ffedb96f6b9f92c12b248
youxia0928-max <youxia0928@gmail.com>
2026-05-11 11:56:08 +0800

    fix: 添加output export配置

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ const nextConfig: NextConfig = {
   // outputFileTracingRoot: path.resolve(__dirname, '../../'), // Uncomment and add 'import path from "path"' if needed
   /* config options here */
   allowedDevOrigins: ['*.dev.coze.site'],
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
